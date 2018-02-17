8684079dfd326f66fecfa37aa86a785a5b6e3d5a
Matiboux <matiboux@gmail.com>
2018-02-17 14:40:02 +0100

    Added website main files

---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="author" content="Matiboux" />
+<meta name="description" content="Portail web non officiel du Lycée Jules Verne" />
+<meta name="keywords" content="portail,portal,web,internet,lycée,high,school,jules,verne,Matiboux" />
+
+<link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/font-awesome.min.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<title>MDL Jules Verne</title>
+
+</head>
+<body>
+
+<div id="header">
+	<div class="container text-center">
+		<h1><b>MDL Jules Verne</b></h1>
+	</div>
+</div>
+
+<div id="main">
+	<div class="container text-justify">
+		<h2>Qu'est ce que la MDL ?</h2>
+		<p>La MDL – ou maison des lycéens – est <b>une association gérée par les lycéen et chargée d'organiser des projets</b> à l'initiative des élèves de l'établissement. La MDL forme également un espace commun à tous les lycéens dans l'établissement lui-même.</p>
+		<hr />
+		
+		<h2>Réseaux sociaux</h2>
+		<ul class="social">
+			<li class="facebook"><a href="https://facebook.com/mdljulesverne/"><i class="fa fa-facebook-square fa-fw"></i> Facebook @mdljulesverne</a></li>
+			<li class="twitter"><a href="https://twitter.com/MDL_ljv/"><i class="fa fa-twitter fa-fw"></i> Twitter @MDL_ljv</a></li>
+			<li class="snapchat"><a href="https://snapchat.com/add/mdl-jv44"><i class="fa fa-snapchat-ghost fa-fw"></i> Snapchat @MDL-jv44</a></li>
+		</ul>
+		<hr />
+		
+		<iframe width="100%" height="400" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBKIllKEjK9ANegaPI4xFMVz6eQicFkJsA&q=place_id:ChIJgYbg7B3sBUgRoRMhqD0pYS4" allowfullscreen></iframe>
+	</div>
+</div>
+
+<div id="footer">
+	<ul>
+		<li><i class="fa fa-heart fa-fw"></i></li>
+		<li>Propulsé par <a href="https://ljv.fr/">ljv.fr</a></li>
+		<li><a href="mailto:mdl.julesverne44@gmail.com" target="_blank">Contactez-nous</a></li>
+	</ul>
+</div>
+
+<?php /*<script type="text/javascript" src="https://cdn.matiboux.com/js/jquery-3.1.1.min.js"></script>*/ ?>
+<?php /*<script type="text/javascript" src="https://cdn.matiboux.com/js/bootstrap.min.js"></script>*/ ?>
+
+</body>
+</html>
