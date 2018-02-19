d1346a9e49c96ccb858fdd205832ef87cbc3b402
Matiboux <matiboux@gmail.com>
2018-02-19 21:51:13 +0100

    Updated footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 	<ul>
 		<li><i class="fa fa-heart fa-fw"></i></li>
 		<li>Propulsé par <a href="https://ljv.fr/">ljv.fr</a></li>
+		<li><a href="https://ljv.fr/about.php" target="_blank">Mentions légales</a></li>
 		<li><a href="mailto:mdl.julesverne44@gmail.com" target="_blank">Contactez-nous</a></li>
 	</ul>
 </div>
