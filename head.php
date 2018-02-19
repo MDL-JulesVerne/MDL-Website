c4ac671c92fa3db49df0f8a3c057ed1a8f9ea7a2
Matiboux <matiboux@gmail.com>
2018-02-19 22:51:22 +0100

    Updated head.php
    - Updated website meta description
    - Updated stylesheet url

--- head.php
+++ head.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="author" content="Matiboux" />
-<meta name="description" content="Portail web non officiel du Lycée Jules Verne" />
-<meta name="keywords" content="portail,portal,web,internet,lycée,high,school,jules,verne,Matiboux" />
+<meta name="description" content="Site web de la MDL du Lycée Jules Verne" />
+<meta name="keywords" content="site,web,internet,mdl,maison,des,lycéens,lycée,high,school,jules,verne,Matiboux" />
 
 <link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/font-awesome.min.css" />
-<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="https://mdl.ljv.fr/style.css" />
