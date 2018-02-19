db90f4cf214258aa5ce40d804bd9daa3900cdadf
Matiboux <matiboux@gmail.com>
2018-02-19 21:51:02 +0100

    Added a menu to header.php

--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <div id="header">
 	<div class="container text-center">
 		<h1><b>MDL Jules Verne</b></h1>
+		
+		<ul class="menu nav nav-pills">
+			<li class="nav-item">
+				<a class="nav-link" href="https://mdl.ljv.fr/comptes-rendus/">Comptes-Rendus</a>
+			</li>
+		</ul>
 	</div>
 </div>
