010610f524bc5f9f289dde3d0c118124742ec8e7
Matiboux <matiboux@gmail.com>
2018-02-21 00:26:32 +0100

    Added parser script to comptes-rendus.php (+ backward comptability)
    Added CSS style to Markdown output

--- comptes-rendus.php
+++ comptes-rendus.php
@@ -1,6 +1,19 @@
+<?php
+if(file_exists('comptes-rendus/' . $_GET['f'] . '.txt')) {
+	header('Content-Type: text/plain');
+	die(file_get_contents('comptes-rendus/' . $_GET['f'] . '.txt'));
+}
+else if(file_exists('comptes-rendus/' . $_GET['f'] . '.md')) {
+	require 'parsedown/Parsedown.php';
+	$Parsedown = new Parsedown();
+	
+	echo '<link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/bootstrap.min.css">' . "\n";
+	echo '<style>body { padding: 10px 20px }</style>' . "\n";
+	die($Parsedown->text(file_get_contents('comptes-rendus/' . $_GET['f'] . '.md')));
+} else { ?>
+
 <html>
 <head>
-
 <?php include 'head.php'; ?>
 <title>MDL Jules Verne</title>
 
@@ -13,19 +26,23 @@
 	<div class="container text-justify">
 		<h2>Comptes-Rendus de la MDL</h2>
 		<ul>
-			<?php $files = glob('/var/www/vhosts/ljv.fr/_fr.ljv.mdl/comptes-rendus/*.txt');
+			<?php $files = glob('comptes-rendus/*.{txt,md}', GLOB_BRACE);
 			rsort($files);
 			foreach($files as $eachFile) { ?>
-				<li><a href="https://mdl.ljv.fr/comptes-rendus/<?=basename($eachFile)?>" target="_blank"><?=basename($eachFile)?></a></li>
+				<li><a href="https://mdl.ljv.fr/comptes-rendus.php?f=<?=urlencode(implode('.', explode('.', basename($eachFile), -1)))?>" target="_blank"><?=implode('.', explode('.', basename($eachFile), -1))?></a></li>
 			<?php } ?>
 		</ul>
+		
+		<p class="text-muted">Affichage des compte-rendus propulsé par <a href="https://github.com/erusev/parsedown">Parsedown</a></p>
 	</div>
 </div>
 
 <?php include 'footer.php'; ?>
 
 <?php /*<script type="text/javascript" src="https://cdn.matiboux.com/js/jquery-3.1.1.min.js"></script>*/ ?>
-<?php /*<script type="text/javascript" src="https://cdn.matiboux.com/js/bootstrap.min.js"></script>*/ ?>
+<?php /*<script type="text/jacascript" src="https://cdn.matiboux.com/js/bootstrap.min.js"></script>*/ ?>
 
 </body>
 </html>
+
+<?php } ?>
