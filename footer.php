f4efb9df70a54158907b8f0283a25746c2267f85
Matiboux <matiboux@gmail.com>
2018-05-01 13:49:52 +0200

    Removed versioning

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<ul>
-		<li>v1.2.0 <i class="fas fa-heart fa-fw"></i> (<a href="https://github.com/MDL-JulesVerne/MDL-Website">open-source</a>)</li>
+		<li><i class="fas fa-heart fa-fw"></i> <a href="https://github.com/MDL-JulesVerne/MDL-Website">Open Source</a></li>
 		<li>Propulsé par <a href="https://ljv.fr/">ljv.fr</a></li>
 		<li><a href="https://ljv.fr/about.php" target="_blank">Mentions légales</a></li>
 		<li><a href="mailto:mdl.julesverne44@gmail.com" target="_blank">Contactez-nous</a></li>
