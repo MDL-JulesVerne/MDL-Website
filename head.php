6558be09c7e3305ad333a80c67c17800064cb7e9
Matiboux <matiboux@gmail.com>
2022-08-27 01:06:23 +0200

    Convert PHP files to Jekyll HTML files

--- head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="author" content="Matiboux" />
-<meta name="description" content="Site web de la MDL du Lycée Jules Verne" />
-<meta name="keywords" content="site,web,internet,mdl,maison,des,lycéens,lycée,high,school,jules,verne,Matiboux" />
-
-<link rel="stylesheet" type="text/css" href="https://cdn.matiboux.com/css/bootstrap.min.css" />
-<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-<link rel="stylesheet" type="text/css" href="style.css" />
